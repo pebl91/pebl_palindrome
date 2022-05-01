0857205465727adf2f0821ef68ea4e6693565229
Pebl <pawel_smusz@yahoo.com>
2022-05-01 17:03:15 +0200

    Finish working and refactored palindrome method

--- lib/pebl_palindrome.rb
+++ lib/pebl_palindrome.rb
@@ -1,6 +1,16 @@
 require "pebl_palindrome/version"
 
-module PeblPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
-end
+class String
+
+  # Returns true for palindroem, false otherwise
+  def palindrome?
+    proccesed_content == proccesed_content.reverse
+  end 
+
+  private 
+
+  # Returns content for palindrome testing.
+  def proccesed_content
+    scan(/[a-z]/i).join.downcase
+  end 
+end 
